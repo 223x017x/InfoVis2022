b8d0b2407fcfe54f0515dd9c66930581f3099bf9
徳野　大智 <223x017x@istc-c-008.epc.center.kobe-u.ac.jp>
2022-05-02 19:48:56 +0900

    task2

--- W06/w06_task2_main.js
+++ W06/w06_task2_main.js
@@ -103,7 +103,7 @@ class ScatterPlot {
 	    .append("text")
 	    .attr("fill", "black")
 	    .attr("text-anchor", "middle")
-	    .attr("x", -30)
+	    .attr("x", -(self.inner_heigh/2))
 	    .attr("y", 50)
 	    .attr("transform", "rotate(-90)")
 	    .attr("font-weight", "middle")
